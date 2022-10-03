e8a472939513e697798e5eaf549dca367a38de10
Shashwat Kandadai <shashwatak@users.noreply.github.com>
2022-10-03 04:02:06 -0700

    improves testing

--- src/main.rs
+++ src/main.rs
@@ -30,11 +30,11 @@ impl FromStr for Cell {
     type Err = ParseCellError;
     fn from_str(cell_str: &str) -> Result<Self, Self::Err> {
         match cell_str {
+            "" => Err(Self::Err::Empty),
+            c if c.len() >= 2 => Err(Self::Err::BadLen),
             "X" => Ok(Cell::X),
             "O" => Ok(Cell::O),
             " " => Ok(Cell::Unmarked),
-            "" => Err(Self::Err::Empty),
-            c if c.len() >= 2 => Err(Self::Err::BadLen),
             _ => Err(Self::Err::BadChar),
         }
     }
@@ -54,23 +54,26 @@ impl fmt::Display for Game {
             .cells
             .iter()
             .enumerate()
-            .map(|val| 
-            match val {
-                (i, cell) if i % 3 == 2 => cell.to_string() + "\n", 
+            .map(|val| match val {
+                (i, cell) if i % 3 == 2 => cell.to_string() + "\n",
                 (_, cell) => cell.to_string(),
             })
             .collect();
-        // for i in 0..9 {
-        //     let cell = &self.cells[i].to_string();
-        //     match i {
-        //         n if n % 3 == 2 => out.push_str(cell),
-        //         _ => out.push_str(cell),
-        //     }
-        // }
         write!(f, "{}", out.join(""))
     }
 }
 
+impl FromStr for Game {
+    type Err = ParseCellError;
+    fn from_str(game_str: &str) -> Result<Self, Self::Err> {
+        let game_string: String = game_str.chars().filter(|c| *c != '\n').collect();
+        match &game_string[..] {
+            "" => Err(Self::Err::Empty),
+            g if g.len() != 9 => Err(Self::Err::BadLen),
+            g => g.chars().enumerate().map(|i, v| ),
+        }
+    }
+}
 fn main() {
     let mut game = Game::default();
     game.cells[0] = Cell::X;
@@ -81,11 +84,63 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
     #[test]
-    fn test() {
-        assert_eq!(1, 1);
+    fn test_bad_cell_to_from_string() {
+        {
+            let cell = "".to_string().parse::<Cell>();
+            assert!(matches!(cell, Err(ParseCellError::Empty)));
+        }
+        {
+            let cell = "OO".to_string().parse::<Cell>();
+            assert!(matches!(cell, Err(ParseCellError::BadLen)));
+        }
+        {
+            let cell = "D".to_string().parse::<Cell>();
+            assert!(matches!(cell, Err(ParseCellError::BadChar)));
+        }
+    }
+
+    #[test]
+    fn test_good_cell_to_from_string() {
+        let good_cells = ["X", "O", " "];
+        for expected_cell in good_cells {
+            let cell = expected_cell.to_string().parse::<Cell>();
+            assert!(matches!(cell, Ok(_)));
+            let result_cell = cell.unwrap().to_string();
+            assert_eq!(expected_cell, result_cell);
+        }
+    }
+
+    #[test]
+    fn test_bad_game_to_from_string() {
+        {
+            let game = "".to_string().parse::<Game>();
+            assert!(matches!(game, Err(ParseCellError::Empty)));
+        }
+        {
+            let game = "XXO\nOO\nXXO".to_string().parse::<Game>();
+            assert!(matches!(game, Err(ParseCellError::BadLen)));
+        }
+        {
+            let game = "XXO\nOOX\nXXXO".to_string().parse::<Game>();
+            assert!(matches!(game, Err(ParseCellError::BadLen)));
+        }
+        {
+            let game = "XXO\nOOD\nXXO".to_string().parse::<Game>();
+            assert!(matches!(game, Err(ParseCellError::BadChar)));
+        }
+    }
+
+    #[test]
+    fn test_game_to_from_string() {
+        let expected_game = "XOX\nOXO\nOXO";
+        let game = expected_game.to_string().parse::<Game>();
+        assert!(matches!(game, Ok(_)));
+        let result_game = game.unwrap().to_string();
+        assert_eq!(expected_game, result_game);
     }
 }
