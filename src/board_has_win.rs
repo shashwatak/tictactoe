f4b65eda7f536f88e363d370999b99e1b777853f
Shashwat Kandadai <shashwatak@users.noreply.github.com>
2022-10-16 01:53:33 -0700

    re-add commented out board_has_win tests

--- src/board_has_win.rs
+++ src/board_has_win.rs
@@ -32,19 +32,19 @@ mod tests {
     use super::*;
     use crate::board::Board;
 
-    // #[test]
-    // fn test_board_with_win() {
-    //     let board = "OOOXXOXXO".to_string().parse::<Board>().unwrap();
-    //     assert!(matches!(board_has_win(&board), Cell::O));
-    //     let board = "OOO    XX".to_string().parse::<Board>().unwrap();
-    //     assert!(matches!(board_has_win(&board), Cell::O));
-    // }
+    #[test]
+    fn test_board_with_win() {
+        let board = "OOOXXOXXO".to_string().parse::<Board>().unwrap();
+        assert!(matches!(board_has_win(&board), Cell::O));
+        let board = "OOO    XX".to_string().parse::<Board>().unwrap();
+        assert!(matches!(board_has_win(&board), Cell::O));
+    }
 
     #[test]
     fn test_board_with_no_win() {
-        // let board = "         ".to_string().parse::<Board>().unwrap();
-        // let cell = board_has_win(&board);
-        // assert!(matches!(cell, Cell::Unmarked));
+        let board = "         ".to_string().parse::<Board>().unwrap();
+        let cell = board_has_win(&board);
+        assert!(matches!(cell, Cell::Unmarked));
         let board = "XXOOXXOOX".to_string().parse::<Board>().unwrap();
         let cell = board_has_win(&board);
         assert!(matches!(cell, Cell::Unmarked));
