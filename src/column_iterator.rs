bac0d91a00f85a963ff8fee9112c576395b97ecb
Shashwat Kandadai <shashwatak@users.noreply.github.com>
2022-10-16 00:37:12 -0700

    column iterator

---
+++ src/column_iterator.rs
@@ -0,0 +1,61 @@
+use crate::board::{NUM_CELLS, NUM_ROWS as COL_SIZE};
+use crate::cell::Cell;
+use crate::cell_iterator::CellIterator;
+
+pub struct ColumnIterator<'a> {
+    count: usize,
+    cells: &'a [Cell; NUM_CELLS],
+}
+
+impl<'a> ColumnIterator<'a> {
+    pub fn new(cells: &'a [Cell; NUM_CELLS]) -> ColumnIterator<'a> {
+        ColumnIterator { count: 0, cells }
+    }
+}
+
+impl<'a> Iterator for ColumnIterator<'a> {
+    type Item = CellIterator<'a>;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count >= COL_SIZE {
+            None
+        } else {
+            let next_column_cell_idx =
+                |column_idx: usize, count: usize| column_idx + (COL_SIZE * count);
+            let column_iterator = CellIterator::new(self.cells, self.count, next_column_cell_idx);
+            self.count += 1;
+            Some(column_iterator)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use crate::board::Board;
+
+    #[test]
+    fn test_column_iterator() {
+        let board = "XOXOXOXXO".to_string().parse::<Board>().unwrap();
+
+        let mut columns = ColumnIterator::new(&board.cells);
+
+        let mut column = columns.next().unwrap();
+        assert!(matches!(column.next().unwrap(), Cell::X));
+        assert!(matches!(column.next().unwrap(), Cell::O));
+        assert!(matches!(column.next().unwrap(), Cell::X));
+        assert!(matches!(column.next(), None));
+
+        let mut column = columns.next().unwrap();
+        assert!(matches!(column.next().unwrap(), Cell::O));
+        assert!(matches!(column.next().unwrap(), Cell::X));
+        assert!(matches!(column.next().unwrap(), Cell::X));
+        assert!(matches!(column.next(), None));
+
+        let mut column = columns.next().unwrap();
+        assert!(matches!(column.next().unwrap(), Cell::X));
+        assert!(matches!(column.next().unwrap(), Cell::O));
+        assert!(matches!(column.next().unwrap(), Cell::O));
+        assert!(matches!(column.next(), None));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 mod board;
 mod board_has_win;
-mod cell_iterator;
-mod row_iterator;
 mod cell;
+mod cell_iterator;
+mod column_iterator;
+mod row_iterator;
 
-use crate::board::{Board, NUM_COLS};
+use crate::board::{Board, NUM_COLS, NUM_ROWS};
 use crate::board_has_win::board_has_win;
 use crate::cell::Cell;
+use crate::column_iterator::ColumnIterator;
 use crate::row_iterator::RowIterator;
 
 fn pretty_print(board: &Board) {
     println!("Row Major:");
     print!("  ");
-    for col_idx in 0..NUM_COLS {
-        print!(" {}", (col_idx + 'a' as usize) as u8 as char);
+    for column_idx in 0..NUM_COLS {
+        print!(" {}", (column_idx + 'a' as usize) as u8 as char);
     }
     println!("\n  -------");
     for (row_idx, row) in RowIterator::new(&board.cells).enumerate() {
@@ -24,20 +26,19 @@ fn pretty_print(board: &Board) {
         println!("\n  -------");
     }
 
-    // println!("Column Major:");
-    // print!("  ");
-    // for row_idx in 0..NUM_ROWS {
-    //     print!(" {}", row_idx + 1);
-    // }
-    // println!("\n  -------");
-    // for col_idx in 0..NUM_COLS {
-    //     print!("{} |", (col_idx + 'a' as usize) as u8 as char);
-    //     let col = CellIterator::new(&board.cells, col_idx, next_col_cell_idx);
-    //     for cell in col {
-    //         print!("{}|", cell);
-    //     }
-    //     println!("\n  -------");
-    // }
+    println!("Column Major:");
+    print!("  ");
+    for row_idx in 0..NUM_ROWS {
+        print!(" {}", row_idx + 1);
+    }
+    println!("\n  -------");
+    for (column_idx, column) in ColumnIterator::new(&board.cells).enumerate() {
+        print!("{} |", (column_idx + 'a' as usize) as u8 as char);
+        for cell in column {
+            print!("{}|", cell);
+        }
+        println!("\n  -------");
+    }
     println!("is win? {}", board_has_win(board));
 }
 
