9e8b6e9b4195c5eab97a673a5ae348412cfc8f2d
Shashwat Kandadai <shashwatak@users.noreply.github.com>
2022-10-19 14:55:57 -0700

    improved parsing chars to indexes, still not working

--- src/user_input.rs
+++ src/user_input.rs
@@ -3,11 +3,13 @@ use crate::board::{NUM_COLS, NUM_ROWS};
 use core::fmt;
 use std::str::FromStr;
 
+#[derive(Debug)]
 pub struct UserInput {
     pub row: usize,
     pub column: usize,
 }
 
+#[derive(Debug)]
 pub enum UserInputError {
     Empty,
     UnparseableInput,
@@ -20,20 +22,25 @@ impl fmt::Display for UserInput {
     }
 }
 
+const MIN_ROW_CHAR: char = '1';
+const MAX_ROW_CHAR: char = ('1' as u8 + NUM_ROWS as u8) as char;
+
+const MIN_COLUMN_CHAR: char = 'a';
+const MAX_COLUMN_CHAR: char = ('a' as u8 + NUM_COLS as u8) as char;
+
 impl FromStr for UserInput {
     type Err = UserInputError;
     fn from_str(input: &str) -> Result<Self, Self::Err> {
         match input.chars().collect::<Vec<char>>()[..] {
             [] => Err(Self::Err::Empty),
             [row_char, column_char] => {
-                let row = row_char as u8 as usize;
-                let column = column_char as u8 as usize;
-                if row >= NUM_ROWS || column >= NUM_COLS {
-                    Err(Self::Err::OutOfBounds)
-                } else {
-                    Ok(UserInput { row, column })
+                if row_char < MIN_ROW_CHAR || row_char > MAX_ROW_CHAR || column_char < MIN_COLUMN_CHAR || column_char > MAX_COLUMN_CHAR {
+                    return Err(Self::Err::OutOfBounds);
                 }
-            },
+                let row = row_char as usize - '1' as usize;
+                let column = column_char as usize - 'a' as usize;
+                Ok(UserInput { row, column })
+            }
             _ => Err(Self::Err::UnparseableInput),
         }
     }
@@ -57,5 +64,13 @@ mod test {
         let out_of_bounds_input = "44";
         let result = out_of_bounds_input.parse::<UserInput>();
         assert!(matches!(result, Err(UserInputError::OutOfBounds)));
+    }
+
+    #[test]
+    fn test_good_inputs() {
+        let input = "a1";
+        let result: UserInput = input.parse().unwrap();
+        assert_eq!(result.row, 0);
+        assert_eq!(result.column, 0);
     }
 }
