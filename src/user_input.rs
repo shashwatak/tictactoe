559c925bf42fb2d839d3782804109021538c3b79
Shashwat Kandadai <shashwatak@users.noreply.github.com>
2022-10-19 15:22:37 -0700

    user input sanitization

--- src/user_input.rs
+++ src/user_input.rs
@@ -23,23 +23,29 @@ impl fmt::Display for UserInput {
 }
 
 const MIN_ROW_CHAR: char = '1';
-const MAX_ROW_CHAR: char = ('1' as u8 + NUM_ROWS as u8) as char;
+const MAX_ROW_CHAR: char = ('1' as u8 + (NUM_ROWS - 1) as u8) as char;
 
 const MIN_COLUMN_CHAR: char = 'a';
-const MAX_COLUMN_CHAR: char = ('a' as u8 + NUM_COLS as u8) as char;
+const MAX_COLUMN_CHAR: char = ('a' as u8 + (NUM_COLS - 1) as u8) as char;
 
 impl FromStr for UserInput {
     type Err = UserInputError;
     fn from_str(input: &str) -> Result<Self, Self::Err> {
         match input.chars().collect::<Vec<char>>()[..] {
             [] => Err(Self::Err::Empty),
-            [row_char, column_char] => {
-                if row_char < MIN_ROW_CHAR || row_char > MAX_ROW_CHAR || column_char < MIN_COLUMN_CHAR || column_char > MAX_COLUMN_CHAR {
-                    return Err(Self::Err::OutOfBounds);
+            [column_char, row_char] => {
+                println!("debug {} {}", row_char, column_char);
+                if row_char >= MIN_ROW_CHAR
+                    && row_char <= MAX_ROW_CHAR
+                    && column_char >= MIN_COLUMN_CHAR
+                    && column_char <= MAX_COLUMN_CHAR
+                {
+                    let row = row_char as usize - '1' as usize;
+                    let column = column_char as usize - 'a' as usize;
+                    Ok(UserInput { row, column })
+                } else {
+                    Err(Self::Err::OutOfBounds)
                 }
-                let row = row_char as usize - '1' as usize;
-                let column = column_char as usize - 'a' as usize;
-                Ok(UserInput { row, column })
             }
             _ => Err(Self::Err::UnparseableInput),
         }
@@ -61,9 +67,11 @@ mod test {
         let result = garbage_input.parse::<UserInput>();
         assert!(matches!(result, Err(UserInputError::UnparseableInput)));
 
-        let out_of_bounds_input = "44";
-        let result = out_of_bounds_input.parse::<UserInput>();
-        assert!(matches!(result, Err(UserInputError::OutOfBounds)));
+        let out_of_bounds_inputs = ["a0", "a4", "d1", "d4"];
+        for input in out_of_bounds_inputs {
+            let result = input.parse::<UserInput>();
+            assert!(matches!(result, Err(UserInputError::OutOfBounds)));
+        }
     }
 
     #[test]
@@ -72,5 +80,17 @@ mod test {
         let result: UserInput = input.parse().unwrap();
         assert_eq!(result.row, 0);
         assert_eq!(result.column, 0);
+        let input = "a3";
+        let result: UserInput = input.parse().unwrap();
+        assert_eq!(result.row, 2);
+        assert_eq!(result.column, 0);
+        let input = "b2";
+        let result: UserInput = input.parse().unwrap();
+        assert_eq!(result.row, 1);
+        assert_eq!(result.column, 1);
+        let input = "c1";
+        let result: UserInput = input.parse().unwrap();
+        assert_eq!(result.row, 0);
+        assert_eq!(result.column, 2);
     }
 }
